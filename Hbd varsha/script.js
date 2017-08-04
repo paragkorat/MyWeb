86719fc13c01b5be7e8b0e7d5865ff7be486635a
Parag Korat <paragkorat74@gmail.com>
2017-08-04 20:01:53 +0530

    Update script.js

--- Hbd varsha/script.js
+++ Hbd varsha/script.js
@@ -36,7 +36,7 @@ var retina = window.devicePixelRatio,
 }(window));
 
 document.addEventListener("DOMContentLoaded", function() {
-  var speed = 100,
+  var speed =200,
       duration = (1.0 / speed),
       confettiRibbonCount = 11,
       ribbonPaperCount = 30,
