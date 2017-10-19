cbf759df5f3833cb806dd22d8bcdd260ac6f525e
Parag Korat <paragkorat74@gmail.com>
2017-10-19 19:05:17 +0530

    Update script.js

--- Happy New Year/script.js
+++ Happy New Year/script.js
@@ -83,9 +83,9 @@ class Firework {
 
     let xDiff = this.targetX - this.x
     let yDiff = this.targetY - this.y
-    if (Math.abs(xDiff) > 0.001 || Math.abs(yDiff) > 0.001) { // is still moving
-      this.x += xDiff / 0.001
-      this.y += yDiff / 0.001
+    if (Math.abs(xDiff) > 0.009 || Math.abs(yDiff) > 0.000) { // is still moving
+      this.x += xDiff / 0.009
+      this.y += yDiff / 0.009
 
       this.history.push({
         x: this.x,
